6c2a2a931c9d1ee2eb9a841307cc90daba0117dc
Rohan Sawant <rohansawantct83@gmail.com>
2018-09-14 16:47:58 +0530

    Get by query set working

--- code/django-server/fras/attendance/views/LectureAttendanceAPI.py
+++ code/django-server/fras/attendance/views/LectureAttendanceAPI.py
@@ -7,3 +7,15 @@ from attendance.serializers.LectureAttendanceSerializer import LectureAttendance
 class LectureAttendanceAPI(viewsets.ModelViewSet):
     queryset = LectureAttendance.objects.all()
     serializer_class = LectureAttendanceSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `id` query parameter in the URL.
+        """
+        queryset = LectureAttendance.objects.all()
+        id = self.request.query_params.get('id', None)
+        print("Query Params", self.request.query_params)
+        if id is not None:
+            queryset = queryset.filter(id=id)
+        return queryset
