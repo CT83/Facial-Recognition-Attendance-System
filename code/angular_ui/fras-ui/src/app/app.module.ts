b81a29b721d260e11e71af051a0c3d769c22011d
CT83 <rohansawantct83@gmail.com>
2018-11-04 17:11:53 +0530

    Angular Sidebar working, updated FACE_API_KEY.json

--- code/angular_ui/fras-ui/src/app/app.module.ts
+++ code/angular_ui/fras-ui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
