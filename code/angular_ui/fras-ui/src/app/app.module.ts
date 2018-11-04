0e8c34f699845e5315969b25112081758fb4fc34
CT83 <rohansawantct83@gmail.com>
2018-11-04 17:14:11 +0530

    Added sidebar

--- code/angular_ui/fras-ui/src/app/app.module.ts
+++ code/angular_ui/fras-ui/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
