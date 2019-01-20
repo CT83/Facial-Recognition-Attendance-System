d88a0861c020c0ec62344a68d2e2da9808ba0723
CT83 <rohansawantct83@gmail.com>
2019-01-20 19:37:01 +0530

    UI Changes

--- code/raspberry-pi-client/run_camera_client.py
+++ code/raspberry-pi-client/run_camera_client.py
@@ -60,30 +60,38 @@ def main():
     image_file = CURRENT_IMAGE_FILE
     person_group_id = FACE_GROUP_ID
     camera = Camera()
+    camera.start_capture()
     face_api_wrapper = FaceAPIWrapper(FACE_API_KEY, FACE_BASE_URL)
 
     print("Capturing Image every ", CAPTURE_INTERVAL)
 
+    i = 0
+
     while 1:
-        image = camera.capture_image()
-        cv2.imwrite(image_file, image)
-        cv2.imshow("Camera Image", image)
-        cv2.waitKey(1)
-        face_ids = face_api_wrapper.detect_faces(image=image_file)
-        if face_ids:
-            # TODO Send the face id to server here.
-            person_ids = \
-                face_api_wrapper.identify_faces(face_ids=face_ids,
-                                                large_person_group=person_group_id)
-            req_ids = [{id} for id in person_ids]
+        try:
+            image = camera.current_frame.read()
+            cv2.imwrite(image_file, image)
+            cv2.imshow("Camera Image", image)
+            cv2.waitKey(1)
+            face_ids = face_api_wrapper.detect_faces(image=image_file)
+            i += 1
+            print(i, "Captured at ", time.time())
+            if face_ids:
+                # TODO Send the face id to server here.
+                person_ids = \
+                    face_api_wrapper.identify_faces(face_ids=face_ids,
+                                                    large_person_group=person_group_id)
+                req_ids = [{id} for id in person_ids]
 
-            requests.post(REST_SERVER_URL + 'time-face-id', data={
-                'lecture_number': get_lecture_number(),
-                'face_ids': req_ids
-            })
-            print("Present IDs:", req_ids)
+                requests.post(REST_SERVER_URL + 'time-face-id', data={
+                    'lecture_number': get_lecture_number(),
+                    'face_ids': req_ids
+                })
+                print("Present IDs:", req_ids)
 
-        time.sleep(CAPTURE_INTERVAL)
+            time.sleep(CAPTURE_INTERVAL)
+        except Exception:
+            pass
 
 
 if __name__ == '__main__':
