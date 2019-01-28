071c5dd2853cd2ca6dc8c0bd99e49b0d4eb9e219
anirudh-iyer <anirudh.si08@gmail.com>
2019-01-29 04:36:32 +0530

    Reformat

--- code/raspberry-pi-client/run_camera_client.py
+++ code/raspberry-pi-client/run_camera_client.py
@@ -12,6 +12,44 @@ from face.FaceAPIWrapper import FaceAPIWrapper
 from utils import get_lecture_number
 
 
+def main():
+    image_file = CURRENT_IMAGE_FILE
+    person_group_id = FACE_GROUP_ID
+    camera = Camera()
+    camera.start_capture()
+    face_api_wrapper = FaceAPIWrapper(FACE_API_KEY, FACE_BASE_URL)
+
+    print("Capturing Image every ", CAPTURE_INTERVAL)
+
+    i = 0
+
+    while 1:
+        try:
+            image = camera.current_frame.read()
+            cv2.imwrite(image_file, image)
+            cv2.imshow("Camera Image", image)
+            cv2.waitKey(1)
+            face_ids = face_api_wrapper.detect_faces(image=image_file)
+            i += 1
+            print(i, "Captured at ", time.time())
+            if face_ids:
+                # TODO Send the face id to server here.
+                person_ids = \
+                    face_api_wrapper.identify_faces(face_ids=face_ids,
+                                                    large_person_group=person_group_id)
+                req_ids = [{id} for id in person_ids]
+
+                requests.post(REST_SERVER_URL + 'time-face-id', data={
+                    'lecture_number': get_lecture_number(),
+                    'face_ids': req_ids
+                })
+                print("Present IDs:", req_ids)
+
+            time.sleep(CAPTURE_INTERVAL)
+        except Exception:
+            pass
+
+
 def add_person(person_name, image_urls, person_group=DEFAULT_PERSON_GROUP):
     print("Name:", person_name)
     print("Images:", image_urls)
@@ -54,44 +92,6 @@ def initial_setup():
     pprint(person_id_dict)
 
     # image_urls = os.listdir("images/Rohan_Sawant/train")
-
-
-def main():
-    image_file = CURRENT_IMAGE_FILE
-    person_group_id = FACE_GROUP_ID
-    camera = Camera()
-    camera.start_capture()
-    face_api_wrapper = FaceAPIWrapper(FACE_API_KEY, FACE_BASE_URL)
-
-    print("Capturing Image every ", CAPTURE_INTERVAL)
-
-    i = 0
-
-    while 1:
-        try:
-            image = camera.current_frame.read()
-            cv2.imwrite(image_file, image)
-            cv2.imshow("Camera Image", image)
-            cv2.waitKey(1)
-            face_ids = face_api_wrapper.detect_faces(image=image_file)
-            i += 1
-            print(i, "Captured at ", time.time())
-            if face_ids:
-                # TODO Send the face id to server here.
-                person_ids = \
-                    face_api_wrapper.identify_faces(face_ids=face_ids,
-                                                    large_person_group=person_group_id)
-                req_ids = [{id} for id in person_ids]
-
-                requests.post(REST_SERVER_URL + 'time-face-id', data={
-                    'lecture_number': get_lecture_number(),
-                    'face_ids': req_ids
-                })
-                print("Present IDs:", req_ids)
-
-            time.sleep(CAPTURE_INTERVAL)
-        except Exception:
-            pass
 
 
 if __name__ == '__main__':
