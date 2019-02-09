34a06ef6cc82bffd0626e2a6fb634375ba0cab69
CT83 <rohansawantct83@gmail.com>
2019-02-09 14:31:36 +0530

    Updated migrations

--- code/raspberry-pi-client/CONSTANTS.py
+++ code/raspberry-pi-client/CONSTANTS.py
@@ -24,6 +24,7 @@ FACE_BASE_URL = 'https://centralindia.api.cognitive.microsoft.com/face/v1.0'
 FACE_PERSON_ID_NAME_DICT = {'da3c26c5-5745-46df-9fa2-8003aed1eff7': 'Tanmay Sawant',
                             '301a333c-2bcf-45b1-b847-fe9191f2ecaa': 'Anirudh Iyer',
                             'f0b07f85-3d65-4ff4-a1f9-41d1d11edb35': 'Rohan Sawant',
+                            'ce72fff6-1643-41ba-b6a6-54ddf2f4cde1': 'Neelam Sawant',
                             }
 FACE_GROUP_ID = 'students'
 
