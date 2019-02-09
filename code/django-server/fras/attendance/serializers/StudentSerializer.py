cca5b87048edaf59f74d4e212fb2cb798115d25f
CT83 <rohansawantct83@gmail.com>
2019-02-09 15:26:47 +0530

    Updated studentserializer

--- code/django-server/fras/attendance/serializers/StudentSerializer.py
+++ code/django-server/fras/attendance/serializers/StudentSerializer.py
@@ -11,6 +11,7 @@ class StudentSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         full_name = validated_data.pop('full_name')
         face_id = validated_data.pop('face_id')
-        student = Student(full_name=full_name, face_id=face_id)
+        id = validated_data.pop('id')
+        student = Student(id=id, full_name=full_name, face_id=face_id)
         student.save()
         return student
